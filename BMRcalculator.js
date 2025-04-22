2bddcd551ae86a09c045a5c1f39bfb4e8d6f4a7b
AakashKhannaatGIT <aakashkhanna.work@gmail.com>
2025-04-22 13:08:42 +0530

    added carbohydrates scale logics

--- BMRcalculator.js
+++ BMRcalculator.js
@@ -8,6 +8,7 @@ const PROTIEN_FOOD_LIST = [
     food: "Egg-Whites",
     calorie: 17,
     qtyInGrams: 58,
+    qty: 1,
     protien: 3.6,
     carbohydrates: 0.2,
     fats: 0.1,
@@ -17,6 +18,7 @@ const PROTIEN_FOOD_LIST = [
     food: "Egg",
     calorie: 72,
     qtyInGrams: 58,
+    qty: 1,
     protien: 6.3,
     carbohydrates: 0.4,
     fats: 4.8,
@@ -26,6 +28,7 @@ const PROTIEN_FOOD_LIST = [
     food: "Chicken breast",
     calorie: 198,
     qtyInGrams: 100,
+    qty: 1,
     protien: 37,
     carbohydrates: 0,
     fats: 4.3,
@@ -35,12 +38,45 @@ const PROTIEN_FOOD_LIST = [
     food: "Soya chunks",
     calorie: 172,
     qtyInGrams: 50,
+    qty: 1,
     protien: 26,
     carbohydrates: 16.5,
     fats: 0.5,
     allowenceQty: 1,
   },
 ];
+const CARBOHYDRATES_FOOD_LIST = [
+  {
+    food: "Egg-Whites",
+    calorie: 17,
+    qtyInGrams: 58,
+    qty: 1,
+    protien: 3.6,
+    carbohydrates: 0.2,
+    fats: 0.1,
+    allowenceQty: 10,
+  },
+];
+const FATS_FOOD_LIST = [];
+
+function totalBMRProcess(person) {
+  const MAINTENCE_CALORIE = getMaintenceCalorie(person);
+  const CALORIE_AND_PROTIEN_GOAL = getCalorieGoalTarget(person);
+  const CALORIE_GOAL = CALORIE_AND_PROTIEN_GOAL.goalCalorie;
+  //Holds the foods that is equally distributed and total calories of protien food
+  const PROTIEN_DISTRIBUTION = distributeProtien(person);
+  console.log(PROTIEN_DISTRIBUTION);
+  const TOTAL_PROTIEN_CALORIE = PROTIEN_DISTRIBUTION.totalProtienCalorie;
+  const UTILIZED_PROTIEN_FOOD_LIST = PROTIEN_DISTRIBUTION.utilizedFoodList;
+
+  const CARBS_DISTRIBUTION = getGoalCarbsTarget(CALORIE_GOAL, person);
+  if (UTILIZED_PROTIEN_FOOD_LIST) {
+    const PROTIEN_FOOD_OBJECT = groupFoodData(UTILIZED_PROTIEN_FOOD_LIST);
+    createFoodParagraph(PROTIEN_FOOD_OBJECT);
+  } else {
+    console.log("BASKET OVERRIDDEN");
+  }
+}
 
 function getPersonBMR(person) {
   const AGE = person.age;
@@ -80,7 +116,7 @@ function getMaintenceCalorie(person) {
   }
   return maintanceCalorie;
 }
-function getGoalCalorieProtienTarget(person) {
+function getCalorieGoalTarget(person) {
   const GOAL = person.goal;
   const WEIGHT = person.weight;
   const MAINTENCE_CALORIE = getMaintenceCalorie(person);
@@ -98,12 +134,121 @@ function getGoalCalorieProtienTarget(person) {
   }
   return { goalCalorie: goalCalorie, protienTarget: protienTarget };
 }
+function getGoalCarbsTarget(calorieGoal, person) {
+  const GOAL = person.goal;
+  let carbsCalories = 0;
+  switch (GOAL) {
+    case "deficit":
+      carbsCalories = calorieGoal % 43;
+      break;
+    case "surplus":
+      carbsCalories = calorieGoal % 45;
+      break;
+  }
+  console.log(carbsCalories);
+}
+function distributeProtien(person) {
+  const CAL_PROTIEN_GOAL = getCalorieGoalTarget(person);
+  const CALORIE_GOAL = CAL_PROTIEN_GOAL.goalCalorie;
+  const PROTIEN_GOAL = CAL_PROTIEN_GOAL.protienTarget;
+  var protienGather = 0;
+  let protienFoodList = [...PROTIEN_FOOD_LIST];
+  let utilizedFoodList = [];
+  let usedFoodQty = [];
+  let totalProtienCalorie = 0;
+  while (protienGather <= PROTIEN_GOAL) {
+    for (let i = 0; i < protienFoodList.length; i++) {
+      let foodItem = protienFoodList[i];
+      if (foodItem.allowenceQty == 0) {
+        if (!usedFoodQty.includes(foodItem.food)) {
+          usedFoodQty.push(foodItem.food);
+        }
+        continue;
+      }
+      protienGather += foodItem.protien;
+      totalProtienCalorie += foodItem.calorie;
+      foodItem.allowenceQty--;
+      utilizedFoodList.push(foodItem);
+    }
+    let allowanceOverride = false;
+    if (usedFoodQty.length == protienFoodList.length) {
+      allowanceOverride = true;
+    } else {
+      allowanceOverride = false;
+    }
+    if (allowanceOverride) {
+      return {
+        utilizedFoodList: false,
+        message: "Basket overridden",
+        basket: usedFoodQty,
+        totalProtienCalorie: 0,
+      };
+    }
+  }
+  return {
+    utilizedFoodList: utilizedFoodList,
+    totalProtienCalorie: totalProtienCalorie,
+  };
+}
+// function distributeCarbohydrates(person,protienCalories) {
+//   const CAL_GOAL = getGoalCalorieTarget(person);
+//   const CALORIE_GOAL = CAL_GOAL.goalCalorie;
+//   const PROTIEN_CAL_GOAL = protienCalories;
+//   if(person.)
+// }
+function groupFoodData(dataArr) {
+  let groupedObject = {};
+  let totalCalorie = 0;
+  let totalProtien = 0;
+  for (let i = 0; i < dataArr.length; i++) {
+    let key = dataArr[i].food;
+    let row = dataArr[i];
+    if (groupedObject[key]) {
+      groupedObject[key].qty += row.qty;
+      groupedObject[key].calorie += row.calorie;
+      groupedObject[key].protien += row.protien;
+      totalCalorie += row.calorie;
+      totalProtien += row.protien;
+    } else {
+      groupedObject[key] = {
+        qty: row.qty,
+        calorie: row.calorie,
+        protien: row.protien,
+        carbohydrates: row.carbohydrates,
+        fats: row.fats,
+        gramQty: row.qtyInGrams,
+      };
+      totalCalorie += row.calorie;
+      totalProtien += row.protien;
+    }
+  }
+  groupedObject.totalNutrition = {
+    totalCalorie: totalCalorie,
+    totalProtien: totalProtien,
+  };
+  return groupedObject;
+}
+function createFoodParagraph(dataObject) {
+  let paragraph = "";
+  for (let key in dataObject) {
+    if (key == "totalNutrition") {
+      continue;
+    }
+    let row = dataObject[key];
+    paragraph += `Food : ${key}\n 1.grams : ${row.gramQty}\n 2.qty :${row.qty}\n Total\n  1.protien :${row.protien}\n  2.Carbohydrates :${row.carbohydrates}\n  3.Fats :${row.fats}\n  4.calories :${row.calorie}\n\n`;
+  }
+  let totalNutrition = dataObject.totalNutrition;
+  paragraph += `Total nutrition\n 1.Calorie : ${parseInt(
+    totalNutrition.totalCalorie
+  )}\n 2.Protien : ${parseInt(totalNutrition.totalProtien)}`;
+  console.log(paragraph);
+}
 
 let person1 = {
-  name: "Aakash Khanna",
-  weight: 93,
-  height: 191,
-  age: 23,
+  name: "Navin derick",
+  weight: 91,
+  height: 186,
+  age: 31,
   gender: "male",
   activityLevel: "lightlyActive",
   goal: "deficit",
@@ -118,42 +263,9 @@ let person2 = {
   activityLevel: "veryActive",
   goal: "surplus",
 };
-
-console.log(distributeNutrition(person1));
-
-function distributeNutrition(person) {
-  const CAL_PROTIEN_GOAL = getGoalCalorieProtienTarget(person);
-  const CALORIE_GOAL = CAL_PROTIEN_GOAL.goalCalorie;
-  const PROTIEN_GOAL = CAL_PROTIEN_GOAL.protienTarget;
-  var protienGather = 0;
-  let protienFoodList = [...PROTIEN_FOOD_LIST];
-  let utilizedFoodList = [];
-  let usedFoodQty = [];
-
-  while (protienGather <= PROTIEN_GOAL) {
-    for (let i = 0; i < protienFoodList.length; i++) {
-      let foodItem = protienFoodList[i];
-      if (foodItem.allowenceQty == 0) {
-        if (!usedFoodQty.includes(foodItem.food)) {
-          usedFoodQty.push(foodItem.food);
-        }
-        continue;
-      }
-      protienGather += foodItem.protien;
-      foodItem.allowenceQty--;
-      utilizedFoodList.push(foodItem);
-    }
-    let allowanceOverride = false;
-    if (usedFoodQty.length == protienFoodList.length) {
-      allowanceOverride = true;
-    } else {
-      allowanceOverride = false;
-    }
-    if (allowanceOverride) {
-      return { message: "Basket overridden", basket: usedFoodQty };
-    }
-  }
-  return utilizedFoodList;
-}
+totalBMRProcess(person1);
+// let protienDataObj = groupFoodData(distributeProtien(person1));
+// let protienCalories = protienDataObj.totalNutrition;
+// createFoodParagraph(protienDataObj);
 // console.log(parseInt(getMaintenceCalorie(person1)));
 // console.log(getMaintenceCalorie(person2));
